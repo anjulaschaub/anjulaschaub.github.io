d6b1a73a187f18b17c8e920d9695f1fc7f7d4cc6
anjulaschaub <anjula.semmens@gmail.com>
2016-02-11 20:45:48 +0100

    working slide-down panel incorporated

--- js/slideacross.js
+++ js/slideacross.js
@@ -1,17 +1,5 @@
 $(document).ready(function(){
-
-
-
-  //D Panel - Toggle a Specic Panel From Event
-
-  $("#d_panel").panel({
-  handle:'#d_panel > .panel_tab',
-  content:'#d_panel > .panel_content',
-  opened:false,
-  direction : "both",
-      toggleEvent : 'click',
-      toggleSelector : '#d_toggle'
-});
-
-
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+    });
 });

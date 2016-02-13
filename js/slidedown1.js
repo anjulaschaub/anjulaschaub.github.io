2c4c11c715efd90f7431c399b079792911c896c4
anjulaschaub <anjula.semmens@gmail.com>
2016-02-14 00:34:46 +0100

    made hover function for index

--- js/slidedown1.js
+++ js/slidedown1.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    $(".col-1").click(function(){
+        $("#panel").slideToggle("slow");
+    });
+});
+
+
+$(document).ready(function(){
     $(".row-buttons").click(function(){
         $("#panel").slideToggle("slow");
     });

dcbb7d5b7581526ca45eae97a1ba95c2e73f4fd3
anjulaschaub <anjula.semmens@gmail.com>
2016-02-13 17:50:28 +0100

    Panel on the left nearly there

---
+++ js/data.js
@@ -0,0 +1,6 @@
+
+  var title = "Haykakan Gyughits";
+  var info = "Haykakan Gyughits is a piece for violin and piano";
+
+  document.getElementById("title").innerHTML= title;
+  document.getElementById("info").innerHTML= info;

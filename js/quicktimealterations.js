2883d139066f924768ce3abd4027cca28a6e1fe4
anjulaschaub <anjula.semmens@gmail.com>
2016-02-15 20:55:58 +0100

    Found a solution for only one audio file playing

--- js/quicktimealterations.js
+++ js/quicktimealterations.js
@@ -27,3 +27,73 @@ function makeNormal() {
    myVideo.height = myVideo.videoHeight;
 
 }
+
+
+
+function playPauseb() {
+
+    var myVideo = document.getElementsByTagName('audio')[1];
+
+    if (myVideo.paused)
+
+        myVideo.play();
+
+    else
+
+        myVideo.pause();
+
+}
+
+function makeBig() {
+
+   var myVideo = document.getElementsByTagName('audio')[1];
+
+   myVideo.height = myVideo.videoHeight * 2;
+
+}
+
+function makeNormal() {
+
+   var myVideo = document.getElementsByTagName('audio')[1];
+
+   myVideo.height = myVideo.videoHeight;
+
+}
+
+
+
+
+
+
+
+
+
+function playPausec() {
+
+    var myVideo = document.getElementsByTagName('audio')[2];
+
+    if (myVideo.paused)
+
+        myVideo.play();
+
+    else
+
+        myVideo.pause();
+
+}
+
+function makeBig() {
+
+   var myVideo = document.getElementsByTagName('audio')[2];
+
+   myVideo.height = myVideo.videoHeight * 2;
+
+}
+
+function makeNormal() {
+
+   var myVideo = document.getElementsByTagName('audio')[2];
+
+   myVideo.height = myVideo.videoHeight;
+
+}

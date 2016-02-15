2fb7c49d9102ca4ac9a345650f1611664f297328
anjulaschaub <anjula.semmens@gmail.com>
2016-02-15 15:45:59 +0100

    fixed row-buttons dash mistake

--- js/tabcollection.js
+++ js/tabcollection.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $("div.rowbuttons div.seehear").click(function(e){
+    $("div.row-buttons div.seehear").click(function(e){
         if (!$(this).hasClass("active")) {
             var tabNum = $(this).index();
             var nthChild = tabNum+1;
-            $("div.rowbuttons div.seehear.active").removeClass("active");
+            $("div.row-buttons div.seehear.active").removeClass("active");
             $(this).addClass("active");
             $("div.col-2wide div.tabcollection.active").removeClass("active");
             $("div.col-2wide div.tabcollection:nth-child("+nthChild+")").addClass("active");

ff4068ec2548e1d69d5f40bd9a0e11eefa086793
anjulaschaub <anjula.semmens@gmail.com>
2016-02-15 21:17:04 +0100

    incorporated 8 pieces of music

--- js/quicktimealterations.js
+++ js/quicktimealterations.js
@@ -1,4 +1,4 @@
-function playPause() {
+function playPause() {                                                      /*FOR FILE ONE*/
 
     var myVideo = document.getElementsByTagName('audio')[0];
 
@@ -30,7 +30,7 @@ function makeNormal() {
 
 
 
-function playPauseb() {
+function playPauseb() {                                                      /*FOR FILE TWO*/
 
     var myVideo = document.getElementsByTagName('audio')[1];
 
@@ -68,7 +68,7 @@ function makeNormal() {
 
 
 
-function playPausec() {
+function playPausec() {                                                      /*FOR FILE THREE*/
 
     var myVideo = document.getElementsByTagName('audio')[2];
 
@@ -93,6 +93,166 @@ function makeBig() {
 function makeNormal() {
 
    var myVideo = document.getElementsByTagName('audio')[2];
+
+   myVideo.height = myVideo.videoHeight;
+
+}
+
+
+
+
+
+function playPaused() {                                                      /*FOR FILE FOUR*/
+
+    var myVideo = document.getElementsByTagName('audio')[3];
+
+    if (myVideo.paused)
+
+        myVideo.play();
+
+    else
+
+        myVideo.pause();
+
+}
+
+function makeBig() {
+
+   var myVideo = document.getElementsByTagName('audio')[3];
+
+   myVideo.height = myVideo.videoHeight * 2;
+
+}
+
+function makeNormal() {
+
+   var myVideo = document.getElementsByTagName('audio')[3];
+
+   myVideo.height = myVideo.videoHeight;
+
+}
+
+
+function playPausee() {                                                      /*FOR FILE FIVE*/
+
+    var myVideo = document.getElementsByTagName('audio')[4];
+
+    if (myVideo.paused)
+
+        myVideo.play();
+
+    else
+
+        myVideo.pause();
+
+}
+
+function makeBig() {
+
+   var myVideo = document.getElementsByTagName('audio')[4];
+
+   myVideo.height = myVideo.videoHeight * 2;
+
+}
+
+function makeNormal() {
+
+   var myVideo = document.getElementsByTagName('audio')[4];
+
+   myVideo.height = myVideo.videoHeight;
+
+}
+
+
+function playPausef() {                                                      /*FOR FILE SIX*/
+
+    var myVideo = document.getElementsByTagName('audio')[5];
+
+    if (myVideo.paused)
+
+        myVideo.play();
+
+    else
+
+        myVideo.pause();
+
+}
+
+function makeBig() {
+
+   var myVideo = document.getElementsByTagName('audio')[5];
+
+   myVideo.height = myVideo.videoHeight * 2;
+
+}
+
+function makeNormal() {
+
+   var myVideo = document.getElementsByTagName('audio')[5];
+
+   myVideo.height = myVideo.videoHeight;
+
+}
+
+
+
+function playPauseg() {                                                      /*FOR FILE SEVEN*/
+
+    var myVideo = document.getElementsByTagName('audio')[6];
+
+    if (myVideo.paused)
+
+        myVideo.play();
+
+    else
+
+        myVideo.pause();
+
+}
+
+function makeBig() {
+
+   var myVideo = document.getElementsByTagName('audio')[6];
+
+   myVideo.height = myVideo.videoHeight * 2;
+
+}
+
+function makeNormal() {
+
+   var myVideo = document.getElementsByTagName('audio')[6];
+
+   myVideo.height = myVideo.videoHeight;
+
+}
+
+
+
+function playPauseh() {                                                      /*FOR FILE EIGHT*/
+
+    var myVideo = document.getElementsByTagName('audio')[7];
+
+    if (myVideo.paused)
+
+        myVideo.play();
+
+    else
+
+        myVideo.pause();
+
+}
+
+function makeBig() {
+
+   var myVideo = document.getElementsByTagName('audio')[7];
+
+   myVideo.height = myVideo.videoHeight * 2;
+
+}
+
+function makeNormal() {
+
+   var myVideo = document.getElementsByTagName('audio')[7];
 
    myVideo.height = myVideo.videoHeight;
 
